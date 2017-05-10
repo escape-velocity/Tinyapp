8e9d55fd221a8cbb901f887707cb0315ae804a1d
Bill <networker747@gmail.com>
2017-05-10 18:45:05 +0000

    start of tinyapp exercise

---
+++ express_server.js
@@ -0,0 +1,52 @@
+
+// var bodyParser = require("body-parser");
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080; // default port 8080
+
+//configuration
+app.set("view engine", "ejs");
+// app.use(bodyParser.urlencoded({extended: true}));
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+//REVIEW funtction logic below!!
+function generateRandomString() {
+  let text = "";
+  let charset = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUWXYZ";
+
+  for (let i = 0; i < 6 ; i++) {
+    text += charset.charAt(Math.floor(Math.random() * charset.length));
+  }
+  return text;
+}
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
